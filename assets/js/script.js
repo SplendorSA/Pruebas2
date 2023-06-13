52361c79bc5671d6495ad50a38a4053ad3b60141
MtrQ <130695828+SplendorSA@users.noreply.github.com>
2023-06-13 17:33:15 -0500

    Prueba

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,22 @@
-window.addEventListener("DOMContentLoaded", function() {
-  var imageList = document.getElementById("image-list");
-  var images = imageList.getElementsByTagName("img");
-  var body = document.body;
-  var currentIndex = 0;
+const imageClassPrefix = 'bg-image';
+const section = document.getElementById('Parara');
 
-  for (var i = 0; i < images.length; i++) {
-    images[i].addEventListener("click", changeBackground);
-  }
+function changeBackground(imageElement, className) {
+    const images = imageElement.parentNode.parentNode.querySelectorAll('img');
+    images.forEach(img => img.classList.remove('selected'));
+    imageElement.classList.add('selected');
 
-  function changeBackground() {
-    var clickedImage = this.src;
-    body.style.backgroundImage = "url('" + clickedImage + "')";
+    section.classList.remove(getCurrentBackgroundClass());
+    section.classList.add(className);
+}
 
-    // Restablecer estilos de todas las imágenes
-    for (var i = 0; i < images.length; i++) {
-      images[i].style.transform = "scale(1)";
+function getCurrentBackgroundClass() {
+    const classList = section.classList;
+    for (let i = 0; i < classList.length; i++) {
+        const className = classList[i];
+        if (className.startsWith(imageClassPrefix)) {
+            return className;
+        }
     }
-
-    // Aplicar estilo a la imagen clickeada
-    this.style.transform = "scale(1.2)";
-  }
-
-  function changeImage() {
-    var nextImage = images[currentIndex].src;
-    body.style.backgroundImage = "url('" + nextImage + "')";
-
-    // Restablecer estilos de todas las imágenes
-    for (var i = 0; i < images.length; i++) {
-      images[i].style.transform = "scale(1)";
-    }
-
-    // Aplicar estilo a la imagen actual
-    images[currentIndex].style.transform = "scale(1.2)";
-
-    currentIndex++;
-    if (currentIndex >= images.length) {
-      currentIndex = 0;
-    }
-  }
-
-  // Llamar a la función de cambio de imágenes inicialmente
-  changeImage();
-
-  // Establecer el intervalo para cambiar automáticamente las imágenes cada 3 segundos
-  setInterval(changeImage, 3000);
-});
+    return 'bg-prueba';
+}
