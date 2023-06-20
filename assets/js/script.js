6b31674cad76ca36276daf40dcf7058b66e805c8
MtrQ <130695828+SplendorSA@users.noreply.github.com>
2023-06-20 16:28:13 -0500

    Pruebas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 const imageClassPrefix = 'bg-image';
-const mobileImageClassPrefix = 'mobile-bg-image';
 const section = document.getElementById('Parara');
 let currentImageIndex = 0;
 const images = [
@@ -9,23 +8,25 @@ const images = [
     'path-to-image4.jpg'
 ];
 
-function changeBackground(imageElement, className, mobileClassName) {
+function changeBackground(imageElement, className) {
     const images = imageElement.parentNode.parentNode.querySelectorAll('img');
     images.forEach(img => img.classList.remove('selected'));
     imageElement.classList.add('selected');
 
-    section.classList.remove(getAllBackgroundClasses());
-    if (isMobileDevice()) {
-        section.classList.add(mobileClassName);
-    } else {
-        section.classList.add(className);
-    }
+    section.classList.remove(getCurrentBackgroundClass());
+    section.classList.add(className);
     currentImageIndex = Array.from(images).indexOf(imageElement);
 }
 
-function getAllBackgroundClasses() {
+function getCurrentBackgroundClass() {
     const classList = section.classList;
-    return Array.from(classList).filter(className => className.startsWith(imageClassPrefix) || className.startsWith(mobileImageClassPrefix));
+    for (let i = 0; i < classList.length; i++) {
+        const className = classList[i];
+        if (className.startsWith(imageClassPrefix)) {
+            return className;
+        }
+    }
+    return 'bg-prueba';
 }
 
 function changeBackgroundAutomatically() {
@@ -37,16 +38,10 @@ function changeBackgroundAutomatically() {
     const imageElements = section.querySelectorAll('.image-list li img');
     const imageElement = imageElements[currentImageIndex];
     const className = 'bg-image' + (currentImageIndex + 1);
-    const mobileClassName = 'mobile-bg-image' + (currentImageIndex + 1);
-    changeBackground(imageElement, className, mobileClassName);
+    changeBackground(imageElement, className);
 }
 
 setInterval(changeBackgroundAutomatically, 5000);
-
-function isMobileDevice() {
-    return window.innerWidth <= 828;
-}
-
 
 
 
